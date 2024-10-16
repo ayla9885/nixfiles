895c12f8d7e47b48a0d3dc6412f85e01432e42e6
ayla <number1noah@gmail.com>
2024-10-15 20:45:32 -0500

    Reformatted program configs Changed programs.foo into programs = { foo } etc.

--- home.nix
+++ home.nix
@@ -91,62 +91,65 @@
   };
 
   # Git 
-  programs.git = {
-    enable = true;
-    userName = "ayla9885";
-    userEmail = "number1noah@gmail.com";
-    extraConfig = {
-      init.defaultBranch = "main";
-      core.editor = "nvim";
-    };
-  };
-
-  programs.fish = {
-    enable = true;
-  };
-  
-  # Starship 
-  programs.starship = {
-    enable = true;
-  };
-
-  # Alacritty 
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      window = {
-        resize_increments = true; # Resize in cell increments
+  programs = {
+    git = {
+      enable = true;
+      userName = "ayla9885";
+      userEmail = "number1noah@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+        core.editor = "nvim";
       };
-      font = {
-        size = 14;
-        normal = {
-          # family = "BigBlueTermPlus Nerd Font Mono";
-          family = "UbuntuMono Nerd Font Mono";
-          # family = "Monofur Nerd Font Mono";
-          # family = "JetBrainsMono Nerd Font Mono";
-          style = "Bold";
+    };
+
+    fish = {
+      enable = true;
+    };
+    
+    # Starship 
+    starship = {
+      enable = true;
+    };
+
+    # Alacritty 
+    alacritty = {
+      enable = true;
+      settings = {
+        window = {
+          resize_increments = true; # Resize in cell increments
+        };
+        font = {
+          size = 14;
+          normal = {
+            # family = "BigBlueTermPlus Nerd Font Mono";
+            family = "UbuntuMono Nerd Font Mono";
+            # family = "Monofur Nerd Font Mono";
+            # family = "JetBrainsMono Nerd Font Mono";
+            style = "Bold";
+          };
         };
       };
     };
-  };
 
-  # Pidgin
-  programs.pidgin = {
-    enable = true;
-    #plugins = with pkgs; [
-      # purple-discord
-    # ];
-  };
-
-  # Librewolf
-  programs.librewolf = {
-    enable = true;
-    settings = {
-      "privacy.donottrackheader.enabled" = true; # Sends websites a "do not track" request
-      "privacy.globalprivacycontrol.enabled" = true; # Asks websites to not sell my data
-      "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = false; # Preserve history after closing  
-      "layout.css.always_underline_links" = true;
+    # Pidgin
+    pidgin = {
+      enable = true;
+      #plugins = with pkgs; [
+        # purple-discord
+      # ];
     };
+
+    # Librewolf
+    librewolf = {
+      enable = true;
+      settings = {
+        "privacy.donottrackheader.enabled" = true; # Sends websites a "do not track" request
+        "privacy.globalprivacycontrol.enabled" = true; # Asks websites to not sell my data
+        "privacy.clearOnShutdown_v2.historyFormDataAndDownloads" = false; # Preserve history after closing  
+        "layout.css.always_underline_links" = true;
+      };
+    };
+
   };
 
   #           #
